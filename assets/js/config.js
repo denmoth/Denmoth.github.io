8bec1cf1c3296646e86e3063bffa699509daeb4d
Denmoth <149831362+denmoth@users.noreply.github.com>
2026-01-09 09:38:46 +0200

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.DENMOTH_CONFIG = {
+    SUPABASE_URL: 'https://dtkmclmaboutpbeogqmw.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a21jbG1hYm91dHBiZW9ncW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNDA4NDUsImV4cCI6MjA4MjYxNjg0NX0.BcfRGmUuOKkAs5KYrLNyoymry1FnY4jqQyCanZ4x-PM',
+    ADMIN_EMAIL: 'denmoth8871top@gmail.com'
+};
+
+// Глобальные переменные состояния
+window.currentUser = null;
+window.isAdmin = false;
