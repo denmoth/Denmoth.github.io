799e2ea3f6849c0d4415551395537e8218ad78ed
Denmoth <149831362+denmoth@users.noreply.github.com>
2026-01-09 10:11:05 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,12 @@
-window.DENMOTH_CONFIG = {
-    SUPABASE_URL: 'https://dtkmclmaboutpbeogqmw.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a21jbG1hYm91dHBiZW9ncW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNDA4NDUsImV4cCI6MjA4MjYxNjg0NX0.BcfRGmUuOKkAs5KYrLNyoymry1FnY4jqQyCanZ4x-PM',
-    ADMIN_EMAIL: 'denmoth8871top@gmail.com'
+window.Denmoth = {
+    Config: {
+        SUPABASE_URL: 'https://dtkmclmaboutpbeogqmw.supabase.co',
+        SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0a21jbG1hYm91dHBiZW9ncW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNDA4NDUsImV4cCI6MjA4MjYxNjg0NX0.BcfRGmUuOKkAs5KYrLNyoymry1FnY4jqQyCanZ4x-PM',
+        ADMIN_EMAIL: 'denmoth8871top@gmail.com'
+    },
+    State: {
+        currentUser: null,
+        isAdmin: false,
+        bannedUsersCache: []
+    }
 };
-
-// Глобальные переменные состояния
-window.currentUser = null;
-window.isAdmin = false;
